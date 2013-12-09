ae456dbfafeefc69ea737b26fd2d082c24894a35
gong023 <gon.gong.gone@gmail.com>
2013-12-10 03:08:39 +0900

    allow access from not same origin

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -1,9 +1,18 @@
 class HistoriesController < ApplicationController
+  before_filter :create_header
+
   def index
     @results = History.page_formatted 1
   end
 
   def show
     @results = History.page_formatted params[:id]
+  end
+
+  private
+  def create_header
+    response.headers['Access-Control-Allow-Origin'] = 'http://localhost'
+    response.headers["Access-Control-Allow-Headers"] = "Content-Type, X-Requested-With"
+    response.headers["Access-Control-Allow-Methods"] = "GET"
   end
 end
--- spec/controllers/histories_controller_spec.rb
+++ spec/controllers/histories_controller_spec.rb
@@ -7,6 +7,12 @@ shared_examples "valid json response" do
   it { expect(parse_json(response.body).pop).to have_key "path" }
 end
 
+shared_examples "allow access not same origin" do
+  it { expect(response.header["Access-Control-Allow-Origin"]).to eq "http://localhost" }
+  it { expect(response.header["Access-Control-Allow-Methods"]).to eq "GET" }
+  it { expect(response.header["Access-Control-Allow-Headers"]).to eq "Content-Type, X-Requested-With" }
+end
+
 shared_context "request with valid history" do
   before { valid_history; request }
 end
@@ -18,6 +24,7 @@ describe HistoriesController do
       let(:request) { get :index, format: 'json' }
       include_context "request with valid history"
       it_behaves_like "valid json response"
+      it_behaves_like "allow access not same origin"
       it { expect(parse_json(response.body).count).to be 20 }
     end
   end
@@ -27,14 +34,8 @@ describe HistoriesController do
       let(:request) { get :show, format: 'json', id: 1}
       include_context "request with valid history"
       it_behaves_like "valid json response"
+      it_behaves_like "allow access not same origin"
       it { expect(parse_json(response.body).count).to be 20 }
-    end
-
-    context "with id 2" do
-      let(:request) { get :show, format: 'json', id: 2}
-      include_context "request with valid history"
-      it_behaves_like "valid json response"
-      it { expect(parse_json(response.body).count).to be 10 }
     end
   end
 end
